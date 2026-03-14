1cead4261a0031bb2ddc14b355fc61dca454eeb4
Jesus David <info@jesusdavid.net>
2026-03-14 18:51:17 +0000

    redirecciones 404

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://eleahora.com',
+  build: {
+    format: 'directory',
+  },
   integrations: [
     svelte(),
     sitemap({
